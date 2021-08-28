8dc93475f800f01f6b11720ebc8b28c0c5a5a3fe
Jeremy Barnes <jeremypb21@gmail.com>
2021-08-28 15:40:07 -0400

    Added score declaration... but increment goes up by two everytime... however, when do a prefix increment, score seems to stay at 0. Need to fix this and add finalscore declaration

--- scripts/rock_paper_scissors.js
+++ scripts/rock_paper_scissors.js
@@ -5,22 +5,13 @@ let playerSelection;
 //userInputs Rock,Paper, or Scissor
 
 //Return message to user to try again if puts in wrong input
-while(playerSelection != "rock" || playerSelection != "paper" || playerSelection != "scissors" || playerInput === null){
-    playerInput = prompt("Let's Play Rock, Paper, Scissors!","Enter Rock, Paper, or Scissors");
-    if (playerInput == null){
-        playerInput = prompt("Let's Play Rock, Paper, Scissors!","Enter Rock, Paper, or Scissors");
-    }
-    else{
-    playerSelection = playerInput.toLowerCase();
-        console.log(playerSelection);
-    }
-    if (playerSelection == "rock" || playerSelection == "paper" || playerSelection == "scissors"){
-        break;
-    }
-}
+
 
 
 function game() {
+let playerScore = 0;
+let computerScore = 0;
+/*let scoreDeclaration = 'The score is ' + playerScore + '-' + computerScore + '.';*/
 for (let i = 0; i < 6; i++){ 
     while(playerSelection != "rock" || playerSelection != "paper" || playerSelection != "scissors" || playerInput === null){
         playerInput = prompt("Let's Play Rock, Paper, Scissors!","Enter Rock, Paper, or Scissors");
@@ -52,11 +43,13 @@ for (let i = 0; i < 6; i++){
     }
     else if(playerSelection == 'rock' && computerSelection == 'paper'  || playerSelection =='paper' && computerSelection=='scissors'
     || playerSelection =='scissors' && computerSelection =='rock'){
-        let loseDeclaration = 'You lose! ' + computerSelection.charAt(0).toUpperCase() + computerSelection.slice(1) + ' beats ' + playerSelection + '.';
+        computerScore = ++computerScore;
+        let loseDeclaration = 'You lose! ' + computerSelection.charAt(0).toUpperCase() + computerSelection.slice(1) + ' beats ' + playerSelection + '. ' + 'The score is ' + playerScore + '-' + computerScore + '.';
         return loseDeclaration;
     }
     else{
-        let winDeclaration = 'You win! ' + playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1) +     ' beats ' + computerSelection + '.';
+        playerScore = ++playerScore;
+        let winDeclaration = 'You win! ' + playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1) +     ' beats ' + computerSelection + '. ' + 'The score is ' + playerScore + '-' + computerScore + '.';
         return winDeclaration;
     }
   }
