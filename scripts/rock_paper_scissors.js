59dc9c3267fc1b0c97f2ad172fc18039aa0ca5ed
Jeremy Barnes <jeremypb21@gmail.com>
2021-11-02 18:15:56 -0400

    11.2.21 had to add conditional for checking winner  within the eventListener function... if outside then doesn't check on click... so only loops through it forEach button...great lesson learned.

--- scripts/rock_paper_scissors.js
+++ scripts/rock_paper_scissors.js
@@ -23,7 +23,7 @@ function game() {
   endGameDiv.textContent = 'First player to five wins!'
   newDiv.appendChild(endGameDiv);
 
- const buttons = document.querySelectorAll('button');
+ const buttons = document.querySelectorAll('button'); //buttons acts similar to array
     buttons.forEach((button) => {
         button.addEventListener('click',()=> {
         let playerSelection = button.id; 
@@ -38,12 +38,13 @@ function game() {
         const computerSelection = computerPlay();
         console.log(computerSelection);
     
-        
+    function checkRound(playerSelection,computerSelection){  
     if(playerSelection == computerSelection){
         
         drawDeclaration = 'Draw! Play again! ' + 'The score is ' + playerScore + '-' + computerScore + '.';
         declaration.textContent = drawDeclaration;
         return drawDeclaration;
+        
     }
     else if(playerSelection == 'rock' && computerSelection == 'paper'  || playerSelection =='paper' && computerSelection=='scissors'
     || playerSelection =='scissors' && computerSelection =='rock'){
@@ -60,12 +61,30 @@ function game() {
         
         return winDeclaration;
     }
+  }
+  checkRound(playerSelection,computerSelection);
+
+  function checkWinner(computerScore,playerScore){
+    if (computerScore>=5){
+        let finalResult = 'Sorry, you lost the game! Refresh the page to try again.';
+        endGameDiv.textContent = finalResult;
+        console.log(finalResult);
+        return finalResult;
+        return checkWinner; //so does not keep changing once one player reaches 5
+    }
+    if (playerScore>=5){
+      let finalResult = 'Congratulations! You won the game! Refresh the page to play again';
+      endGameDiv.textContent = finalResult;
+      console.log(finalResult);
+      return finalResult;
+      return checkWinner;
+    }
+  }
+  checkWinner(computerScore,playerScore);
   }); //for eventListener
   
-
-//For some reason computerScore is not logging in console... Out of scope...? But working with declarations
-//11.2 Still not changing endGameDiv.textContent...so function is not seeing the conditional...?
-
+  
+/* 11.2.21 had to add this within the eventListener function... if outside then doesn't check on click... so only loops through it forEach button...great lesson learned.
   if (computerScore>=5){
       let finalResult = 'Sorry, you lost the game! Refresh the page to try again.';
       endGameDiv.textContent = finalResult;
@@ -77,9 +96,9 @@ function game() {
     endGameDiv.textContent = finalResult;
     console.log(finalResult);
     return finalResult;
-  }
+  }*/
  }); //for.Each function... the variables are declared within game function... but inner function can see computerScore...
-//11.2.21 want to try to make seperate function within game() for conditional... so move for.Each to close after eventListener
+
 }
 game();
 
